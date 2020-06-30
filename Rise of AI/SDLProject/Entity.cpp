cc63216af54471e09b3840fcef93f3d31b7e2411
Simon <simonoh54@icloud.com>
2020-06-30 00:10:59 -0700

    Rise of AI

---
+++ Rise of AI/SDLProject/Entity.cpp
@@ -0,0 +1,254 @@
+#include "Entity.h"
+
+Entity::Entity()
+{
+    position = glm::vec3(0);
+    movement = glm::vec3(0);
+    velocity = glm::vec3(0);
+    acceleration = glm::vec3(0);
+    speed = 0;
+    
+    modelMatrix = glm::mat4(1.0f);
+}
+
+bool Entity::CheckCollision(Entity *other) {
+    if (isActive == false or other->isActive == false) {
+        return false;
+    }
+    float xdist = fabs(position.x - other->position.x) - ((width + other->width) / 2.0f);
+    float ydist = fabs(position.y - other->position.y) - ((height + other->height) / 2.0f);
+    
+    if (xdist < 0 and ydist < 0) {
+        return true;
+    }
+    return false;
+}
+
+void Entity::CheckCollisionsX(Entity *objects, int objectCount) {
+    for (int i = 0; i < objectCount; i++) {
+        Entity *object = &objects[i];
+        if (CheckCollision(object)) {
+            float xdist = fabs(position.x - object->position.x);
+            float penetrationX = fabs(xdist - (width / 2.0f) - (object->width / 2.0f));
+            if (velocity.x > 0) {
+                position.x -= penetrationX;
+                velocity.x = 0;
+                collidedRight = true;
+                if (object->entityType == ENEMY) {
+                    isActive = false;
+                    std::cout << "You are dead\n";
+                }
+            }
+            else if (velocity.x < 0) {
+                position.x += penetrationX;
+                velocity.x = 0;
+                collidedLeft = true;
+                if (object->entityType == ENEMY) {
+                    isActive = false;
+                    std::cout << "You are dead\n";
+                }
+            }
+        }
+    }
+}
+
+void Entity::CheckCollisionsY(Entity *objects, int objectCount) {
+    for (int i = 0; i < objectCount; i++) {
+        Entity *object = &objects[i];
+        if (CheckCollision(object)) {
+            float ydist = fabs(position.y - object->position.y);
+            float penetrationY = fabs(ydist - (height / 2.0f) - (object->height / 2.0f));
+            if (velocity.y > 0) {
+                position.y -= penetrationY;
+                velocity.y = 0;
+                collidedTop = true;
+            }
+            else if (velocity.y < 0) {
+                position.y += penetrationY;
+                velocity.y = 0;
+                collidedBottom = true;
+                if (object->entityType != PLATFORM and object->entityType != PLAYER) {
+                    object->isActive = false;
+                }
+            }
+        }
+    }
+}
+
+void Entity::AIWalker() {
+    switch (aiState) {
+        case IDLE:
+            break;
+        case WALKING:
+            if (position.x <= -4.8f) {
+                movement = glm::vec3(1, 0, 0);
+            }
+            if (position.x >= 4.8f) {
+                movement = glm::vec3(-1, 0, 0);
+            }
+        case FOLLOWING:
+            break;
+        case ATTACKING:
+            if (movedSpaces >= 1.3f) {
+                movedSpaces = 0;
+                jump = false;
+                velocity.y = jumpPower;
+            }
+            if (position.x <= -4.8f) {
+                movement = glm::vec3(1, 0, 0);
+            }
+            if (position.x >= 4.8f) {
+                movement = glm::vec3(-1, 0, 0);
+            }
+    }
+}
+
+void Entity::AIWait(Entity *player) {
+    switch (aiState) {
+        case IDLE:
+            if (glm::distance(position, player->position) < 3.0f) {
+                aiState = FOLLOWING;
+            }
+            break;
+        case WALKING:
+            break;
+        case FOLLOWING:
+            if (player->position.x < position.x) {
+                movement = glm::vec3(-1, 0, 0);
+            }
+            else {
+                movement = glm::vec3(1, 0, 0);
+            }
+            break;
+        case ATTACKING:
+            break;
+    }
+}
+
+void Entity::AI(Entity *player) {
+    switch(aiType) {
+        case WALKER:
+            AIWalker();
+            break;
+        case WAIT:
+            AIWait(player);
+            break;
+    }
+}
+
+void Entity::Update(float deltaTime, Entity *player, Entity *enemies, Entity *platforms, int enemyCount, int platformCount) {
+    if (isActive == false) {
+        return;
+    }
+    
+    collidedTop = false;
+    collidedBottom = false;
+    collidedLeft = false;
+    collidedRight = false;
+    
+    if (entityType == ENEMY) {
+        AI(player);
+    }
+    
+    if (animIndices != NULL) {
+        if (glm::length(movement) != 0) {
+            animTime += deltaTime;
+
+            if (animTime >= 0.25f) {
+                animTime = 0.0f;
+                animIndex++;
+                if (animIndex >= animFrames) {
+                    animIndex = 0;
+                }
+            }
+        }
+        else {
+            animIndex = 0;
+        }
+    }
+    
+    if (jump) {
+        jump = false;
+        velocity.y = jumpPower;
+    }
+    
+    velocity.x =  movement.x * speed;
+    velocity += acceleration * deltaTime;
+    position += velocity * deltaTime;
+    
+    position.y += velocity.y * deltaTime;
+    CheckCollisionsY(platforms, platformCount);
+    
+    position.x += velocity.x * deltaTime;
+    movedSpaces += abs(velocity.x * deltaTime);
+    CheckCollisionsX(platforms, platformCount);
+    
+    if (entityType == PLAYER) {
+        CheckCollisionsX(enemies, enemyCount);
+        CheckCollisionsY(enemies, enemyCount);
+        if (isActive == false) {
+            for (int i = 0; i < enemyCount; i++) {
+                enemies[i].movement = glm::vec3(0, 0, 0);
+            }
+        }
+    }
+    
+    modelMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::translate(modelMatrix, position);
+}
+
+void Entity::DrawSpriteFromTextureAtlas(ShaderProgram *program, GLuint textureID, int index)
+{
+    float u = (float)(index % animCols) / (float)animCols;
+    float v = (float)(index / animCols) / (float)animRows;
+    
+    float width = 1.0f / (float)animCols;
+    float height = 1.0f / (float)animRows;
+    
+    float texCoords[] = { u, v + height, u + width, v + height, u + width, v,
+        u, v + height, u + width, v, u, v};
+    
+    float vertices[]  = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    
+    glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->positionAttribute);
+    
+    glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program->texCoordAttribute);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    glDisableVertexAttribArray(program->positionAttribute);
+    glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
+void Entity::Render(ShaderProgram *program) {
+    if (isActive == false) {
+        return;
+    }
+    
+    program->SetModelMatrix(modelMatrix);
+    
+    if (animIndices != NULL) {
+        DrawSpriteFromTextureAtlas(program, textureID, animIndices[animIndex]);
+        return;
+    }
+    
+    float vertices[]  = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    
+    glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->positionAttribute);
+    
+    glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program->texCoordAttribute);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    glDisableVertexAttribArray(program->positionAttribute);
+    glDisableVertexAttribArray(program->texCoordAttribute);
+}
---
+++ Rise of AI/SDLProject/main.cpp
@@ -0,0 +1,348 @@
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "Entity.h"
+
+#define PLATFORM_COUNT 15
+#define ENEMY_COUNT 3
+
+struct GameState {
+    Entity *player;
+    Entity *platforms;
+    Entity *enemies;
+};
+
+GameState state;
+
+SDL_Window* displayWindow;
+bool gameIsRunning = true;
+
+ShaderProgram program;
+glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
+
+GLuint LoadTexture(const char* filePath) {
+    int w, h, n;
+    unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+    
+    if (image == NULL) {
+        std::cout << "Unable to load image. Make sure the path is correct\n";
+        assert(false);
+    }
+    
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    
+    stbi_image_free(image);
+    return textureID;
+}
+
+
+void Initialize() {
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("2D Platformer Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
+    
+#ifdef _WINDOWS
+    glewInit();
+#endif
+    
+    glViewport(0, 0, 640, 480);
+    
+    program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+    
+    viewMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::mat4(1.0f);
+    projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+    
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+    
+    glUseProgram(program.programID);
+    
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    glEnable(GL_BLEND);
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+   
+    // Initialize Game Objects
+    
+    // Initialize Player
+    state.player = new Entity();
+    state.player->entityType = PLAYER;
+    state.player->position = glm::vec3(-4, -2, 0);
+    state.player->movement = glm::vec3(0);
+    state.player->acceleration = glm::vec3(0, -9.81f, 0);
+    state.player->speed = 1.5f;
+    state.player->textureID = LoadTexture("george_0.png");
+    
+    state.player->animRight = new int[4] {3, 7, 11, 15};
+    state.player->animLeft = new int[4] {1, 5, 9, 13};
+    state.player->animUp = new int[4] {2, 6, 10, 14};
+    state.player->animDown = new int[4] {0, 4, 8, 12};
+
+    state.player->animIndices = state.player->animRight;
+    state.player->animFrames = 4;
+    state.player->animIndex = 0;
+    state.player->animTime = 0;
+    state.player->animCols = 4;
+    state.player->animRows = 4;
+    state.player->height = 0.8f;
+    state.player->width = 0.65f;
+    state.player->jumpPower = 5.0f;
+    
+    state.platforms = new Entity[PLATFORM_COUNT];
+    
+    GLuint platformTextureID = LoadTexture("platformPack_tile001.png");
+    
+    for (int i = 0; i < PLATFORM_COUNT; i++) {
+        state.platforms[i].entityType = PLATFORM;
+        state.platforms[i].textureID = platformTextureID;
+        if (i < 12) {
+            state.platforms[i].position = glm::vec3(-5 + i, -3.25f, 0);
+        }
+        else {
+            state.platforms[i].position = glm::vec3(-11 + i, -1, 0);
+        }
+    }
+    
+    for (int i = 0; i < PLATFORM_COUNT; i++) {
+        state.platforms[i].Update(0, NULL, NULL, NULL, 0, 0);
+    }
+    
+    state.enemies = new Entity[ENEMY_COUNT];
+    GLuint enemyTextureID = LoadTexture("ctg.png");
+    
+    state.enemies[0].entityType = ENEMY;
+    state.enemies[0].textureID = enemyTextureID;
+    state.enemies[0].position = glm::vec3(3, 0, 0);
+    state.enemies[0].movement = glm::vec3(1, 0, 0);
+    state.enemies[0].speed = 0.7;
+    state.enemies[0].aiType = WALKER;
+    state.enemies[0].aiState = WALKING;
+    state.enemies[0].acceleration = glm::vec3(0, -9.81f, 0);
+    
+    state.enemies[1].entityType = ENEMY;
+    state.enemies[1].textureID = enemyTextureID;
+    state.enemies[1].position = glm::vec3(4, -2.25f, 0);
+    state.enemies[1].speed = 0.8;
+    state.enemies[1].aiType = WAIT;
+    state.enemies[1].aiState = IDLE;
+    state.enemies[1].acceleration = glm::vec3(0, -9.81f, 0);
+
+    state.enemies[2].entityType = ENEMY;
+    state.enemies[2].textureID = enemyTextureID;
+    state.enemies[2].position = glm::vec3(2, -2.25f, 0);
+    state.enemies[2].movement = glm::vec3(1, 0, 0);
+    state.enemies[2].speed = 0.65;
+    state.enemies[2].aiType = WALKER;
+    state.enemies[2].aiState = ATTACKING;
+    state.enemies[2].acceleration = glm::vec3(0, -9.81f, 0);
+    state.enemies[2].jumpPower = 3.0f;
+}
+
+void ProcessInput() {
+    
+    state.player->movement = glm::vec3(0);
+    
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+            case SDL_WINDOWEVENT_CLOSE:
+                gameIsRunning = false;
+                break;
+                
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                    case SDLK_LEFT:
+                        // Move the player left
+                        break;
+                        
+                    case SDLK_RIGHT:
+                        // Move the player right
+                        break;
+                        
+                    case SDLK_SPACE:
+                        if (state.player-> collidedBottom) {
+                            state.player->jump = true;
+                        }
+                        break;
+                }
+                break; // SDL_KEYDOWN
+        }
+    }
+    
+    const Uint8 *keys = SDL_GetKeyboardState(NULL);
+
+    if (keys[SDL_SCANCODE_LEFT]) {
+        state.player->movement.x = -1.0f;
+        state.player->animIndices = state.player->animLeft;
+    }
+    else if (keys[SDL_SCANCODE_RIGHT]) {
+        state.player->movement.x = 1.0f;
+        state.player->animIndices = state.player->animRight;
+    }
+    
+
+    if (glm::length(state.player->movement) > 1.0f) {
+        state.player->movement = glm::normalize(state.player->movement);
+    }
+
+}
+
+#define FIXED_TIMESTEP 0.0166666f
+float lastTicks = 0;
+float accumulator = 0.0f;
+
+void Update() {
+    float ticks = (float)SDL_GetTicks() / 1000.0f;
+    float deltaTime = ticks - lastTicks; lastTicks = ticks;
+    deltaTime += accumulator;
+    if (deltaTime < FIXED_TIMESTEP) {
+        accumulator = deltaTime;
+        return;
+    }
+    while (deltaTime >= FIXED_TIMESTEP) {
+        // Update. Notice it's FIXED_TIMESTEP. Not deltaTime
+        state.player->Update(FIXED_TIMESTEP, state.player, state.enemies, state.platforms, ENEMY_COUNT, PLATFORM_COUNT);
+        
+        for (int i = 0; i < ENEMY_COUNT; i++) {
+            state.enemies[i].Update(FIXED_TIMESTEP, state.player, NULL, state.platforms, 0, PLATFORM_COUNT);
+        }
+        
+        deltaTime -= FIXED_TIMESTEP;
+    }
+    accumulator = deltaTime;
+}
+
+void Render() {
+    bool allEnemiesDead = true;
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    for (int i = 0; i < PLATFORM_COUNT; i++) {
+        state.platforms[i].Render(&program);
+    }
+    
+    for (int i = 0; i < ENEMY_COUNT; i++) {
+        state.enemies[i].Render(&program);
+    }
+    
+    state.player->Render(&program);
+    
+    if (state.player->isActive == false) {
+        GLuint fontID = LoadTexture("Font.png");
+        
+        int characterIndex[] = {71, 97, 109, 101, 79, 118, 101, 114};
+        float originVertices[]  = { -3.0, 4.0, -2.0, 4.0, -2.0, 5.0, -3.0, 4.0, -2.0, 5.0, -3.0, 5.0 };
+        float offset = 0;
+        float yCoords = 4.0;
+        
+        for (int i = 0; i < 8; i++) {
+            float u = (float)(characterIndex[i] % 16) / 16.0f;
+            float v = (float)(characterIndex[i] / 16) / 16.0f;
+
+            float width = 1.0f / 16.0f;
+            float height = 1.0f / 16.0f;
+            
+            if (i == 4) {
+                yCoords -= 1.0;
+                offset = -4;
+            }
+
+            float texCoords[] = { u, v + height, u + width, v + height, u + width, v, u, v + height, u + width, v, u, v};
+            float vertices[]  = { originVertices[0] + i + offset - state.enemies[2].position.x, yCoords, originVertices[2] + i + offset - state.enemies[2].position.x, yCoords, originVertices[4] + i + offset - state.enemies[2].position.x, yCoords + 1, originVertices[6] + i + offset - state.enemies[2].position.x, yCoords, originVertices[8] + i + offset - state.enemies[2].position.x, yCoords + 1, originVertices[10] + i + offset - state.enemies[2].position.x, yCoords + 1 };
+
+            glBindTexture(GL_TEXTURE_2D, fontID);
+
+            glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+            glEnableVertexAttribArray(program.positionAttribute);
+
+            glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+            glEnableVertexAttribArray(program.texCoordAttribute);
+
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+
+            glDisableVertexAttribArray(program.positionAttribute);
+            glDisableVertexAttribArray(program.texCoordAttribute);
+        }
+    }
+    
+    for (int i = 0; i < ENEMY_COUNT; i++) {
+        if (state.enemies[i].isActive == true) {
+            allEnemiesDead = false;
+        }
+    }
+    
+    if (allEnemiesDead == true) {
+        GLuint fontID = LoadTexture("Font.png");
+        
+        int characterIndex[] = {89, 111, 117, 0, 119, 105, 110};
+        float originVertices[]  = { -3.0, 4.0, -2.0, 4.0, -2.0, 5.0, -3.0, 4.0, -2.0, 5.0, -3.0, 5.0 };
+        float offset = 0;
+        float yCoords = 4.0;
+        
+        for (int i = 0; i < 7; i++) {
+            float u = (float)(characterIndex[i] % 16) / 16.0f;
+            float v = (float)(characterIndex[i] / 16) / 16.0f;
+
+            float width = 1.0f / 16.0f;
+            float height = 1.0f / 16.0f;
+
+            float texCoords[] = { u, v + height, u + width, v + height, u + width, v, u, v + height, u + width, v, u, v};
+            float vertices[]  = { originVertices[0] + i + offset - state.player->position.x, yCoords, originVertices[2] + i + offset - state.player->position.x, yCoords, originVertices[4] + i + offset - state.player->position.x, yCoords + 1, originVertices[6] + i + offset - state.player->position.x, yCoords, originVertices[8] + i + offset - state.player->position.x, yCoords + 1, originVertices[10] + i + offset - state.player->position.x, yCoords + 1 };
+
+            glBindTexture(GL_TEXTURE_2D, fontID);
+
+            glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+            glEnableVertexAttribArray(program.positionAttribute);
+
+            glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+            glEnableVertexAttribArray(program.texCoordAttribute);
+
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+
+            glDisableVertexAttribArray(program.positionAttribute);
+            glDisableVertexAttribArray(program.texCoordAttribute);
+        }
+    }
+    
+    SDL_GL_SwapWindow(displayWindow);
+}
+
+
+void Shutdown() {
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    Initialize();
+    
+    while (gameIsRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
+    
+    Shutdown();
+    return 0;
+}
